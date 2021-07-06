75416345261ce1301961e9ea25ec4687b5ee71cd
NeginSal <neginsaljooghi@gmail.com>
2021-07-06 15:20:07 +0430

    adding routes

--- src/router/index.js
+++ src/router/index.js
@@ -1,19 +1,39 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import Item1 from '../views/Item1.vue'
+import Item2 from '../views/Item2.vue'
+import Item3 from '../views/Item3.vue'
+import Item4 from '../views/Item4.vue'
+import Item5 from '../views/Item5.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
   {
     path: '/',
-    name: 'Home',
-    component: Home
+    name: 'Item1',
+    component: Item1
   },
   {
-    path: '/about',
-    name: 'About',
-  }
+    path: '/item2',
+    name: 'Item2',
+    component: Item2
+  },
+  {
+    path: '/item3',
+    name: 'Item3',
+    component: Item3
+  },
+  {
+    path: '/item4',
+    name: 'Item4',
+    component: Item4
+  },
+  {
+    path: '/item5',
+    name: 'Item5',
+    component: Item5
+  },
 ]
 
 const router = new VueRouter({
